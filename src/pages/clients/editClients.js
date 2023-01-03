5a938bca8d750967f9b3bb2f8d8290f77eec21a9
sanjsi0507 <sanjsi0507@gmail.com>
2023-01-03 21:40:05 +0530

    changes

--- src/pages/clients/editClients.js
+++ src/pages/clients/editClients.js
@@ -38,11 +38,7 @@ const EditClients = (props) => {
 
     const getCountry = () => {
         axios.get(`${process.env.REACT_APP_BASE_URL}/countries/show/all`)
-<<<<<<< HEAD
             .then(res => {
-=======
-            .then(res=>{
->>>>>>> 0810c3a5dd00a9220853ddd1f41ac7fba6289ccf
                 setCountry(res.data.data)
             })
             .catch((error) => {
@@ -52,11 +48,7 @@ const EditClients = (props) => {
 
     const getCurrency = () => {
         axios.get(`${process.env.REACT_APP_BASE_URL}/currencies/show/all`)
-<<<<<<< HEAD
             .then(res => {
-=======
-            .then(res=>{
->>>>>>> 0810c3a5dd00a9220853ddd1f41ac7fba6289ccf
                 setCurrency(res.data.data)
             })
             .catch((error) => {
@@ -66,11 +58,7 @@ const EditClients = (props) => {
 
     const getPort = () => {
         axios.get(`${process.env.REACT_APP_BASE_URL}/ports/show/all`)
-<<<<<<< HEAD
             .then(res => {
-=======
-            .then(res=>{
->>>>>>> 0810c3a5dd00a9220853ddd1f41ac7fba6289ccf
                 setPort(res.data.data)
             })
             .catch((error) => {
@@ -111,10 +99,7 @@ const EditClients = (props) => {
                     is_active: data[0].is_active,
                     address: data[0].address
                 })
-<<<<<<< HEAD
                 console.log("current ID", data[0].country_id);
-=======
->>>>>>> 0810c3a5dd00a9220853ddd1f41ac7fba6289ccf
                 setCountryselect(data[0].country_id)
                 setCurrencyselect(data[0].currency_id)
                 setPortselect(data[0].port_id)
@@ -133,7 +118,6 @@ const EditClients = (props) => {
         });
     }
 
-<<<<<<< HEAD
     const submitEdit = () => {
         axios.post(`${process.env.REACT_APP_BASE_URL}/clients/store?client_code=${values.client_code}&client_name=${values.client_name}&sub_code=${values.sub_code}&country_id=${countryselect}&port_id=${portselect}&currency_id=${currencyselect}&email=${values.email}&telephone_number=${values.telephone_number}&fax=${values.fax}&mobile_number=${values.mobile_number}&contact_name=${values.contact_name}&address=${values.address}&image&remarks=${values.remarks}&is_active=${activeselect}&id=${id}`)
             .then(res => {
@@ -146,72 +130,6 @@ const EditClients = (props) => {
     }
 
     const onBack = () => {
-=======
-    const [alertSuccess, setAlertSucces] = useState(true);
-    const [alertFaild, setAlertFaild] = useState(true);
-    useEffect(() => {
-        SuccessMsg('ArrivalNoticies', true, 'error');
-        setTimeout(() => {
-            SuccessMsg('ArrivalNoticies', false, 'error');
-        }, 500);
-    });
-
-    function isFormValidate() {
-        if (
-            !values.client_code ||
-            !values.client_name ||
-            !values.sub_code ||
-            !values.email ||
-            !values.telephone_number ||
-            !values.fax ||
-            !countryselect ||
-            !portselect ||
-            !currencyselect ||
-            !values.telephone_number ||
-            !values.contact_name ||
-            !values.address ||
-            !activeselect
-        ) {
-            return false;
-        }
-
-        return true;
-    }
-
-    const onEdit = (event) => {
-        let clientsobj = {
-            client_code:values.client_code,
-            client_name:values.client_name,
-            sub_code:values.sub_code,
-            countryselect:countryselect,
-            portselect:portselect,
-            currencyselect:currencyselect,
-            email:values.email,
-            telephone_number:values.telephone_number,
-            fax:values.fax,
-            mobile_number:values.mobile_number,
-            contact_name:values.contact_name,
-            address:values.address,
-            remarks:values.remarks,
-            activeselect: activeselect,
-            id:id
-        };
-        if (isFormValidate) {
-            event.preventDefault();
-            editClientApiCall(clientsobj).then((editRes) => {
-                if (editRes.status === 200) {
-                    showAllClientApi();
-                    history.push('/clients');
-                    setAlertSucces(false);
-                } else {
-                    setAlertFaild(false);
-                }
-            });
-        }
-    };
-
-    const onBack = () =>{
->>>>>>> 0810c3a5dd00a9220853ddd1f41ac7fba6289ccf
         history.push('/clients')
     }
 
@@ -229,7 +147,6 @@ const EditClients = (props) => {
                 </Col>
             </Row>
 
-<<<<<<< HEAD
             <Row>
                 <Col lg={6}>
                     <Card>
@@ -264,39 +181,6 @@ const EditClients = (props) => {
             </Row>
             <Button color="primary" type="submit" onClick={() => submitEdit()}>Edit</Button>&nbsp;
             <Button color="danger" type="submit" onClick={onBack}>Back</Button>
-=======
-            <Card>
-                <CardBody>
-                    <AvForm onSubmit={onEdit}>
-
-                        <Row container item spacing={2}>
-                            <Col lg={4}><AvField name="client_code" label="Client Code" type="text" required onChange={handleChange} value={values.client_code}/></Col>
-                            <Col lg={4}><AvField name="client_name" label="Client Name" type="text" required onChange={handleChange} value={values.client_name}/></Col>
-                            <Col lg={4}><AvField name="contact_name" label="Contact Name" type="text" required onChange={handleChange} value={values.contact_name}/></Col>
-                            <Col lg={4}><AvField name="sub_code" label="Sub Code" type="text" required onChange={handleChange} value={values.sub_code}/></Col>
-                            <Col lg={4}><AvField name="email" label="Email" type="email" required onChange={handleChange} value={values.email}/></Col>
-                            <Col lg={4}><AvField name="telephone_number" label="Telephone Number" type="text" required onChange={handleChange} value={values.telephone_number}/></Col>
-                            <Col lg={4}><AvField name="mobile_number" label="Mobile Number" type="text" required onChange={handleChange} value={values.mobile_number}/></Col>
-                            <Col lg={4}><AvField name="fax" label="Fax" type="text" required onChange={handleChange} value={values.fax}/></Col>            
-                            <Col lg={4}><AvField name="address" label="Address" type="text" required onChange={handleChange} value={values.address}/></Col>
-                            <Col lg={4}><InputLabel id="demo-simple-select-label">Country</InputLabel><Select labelId="demo-simple-select-label" id="demo-simple-select" value={countryselect} onChange={changeCountry} sx={{  width: '100%', height:36 , mb: 2 }}>{country.map((con) => (<MenuItem value={con.id} key={con.id}>{con.country_name}</MenuItem>))}</Select></Col>
-                            <Col lg={4}><InputLabel id="demo-simple-select-label">Port</InputLabel><Select labelId="demo-simple-select-label" id="demo-simple-select" value={portselect} onChange={changePort} sx={{ width: '100%', height:36 , mb: 2 }}>{port.map((por) => (<MenuItem value={por.id} key={por.id}>{por.port_name}</MenuItem>))}</Select></Col>
-                            <Col lg={4}><InputLabel id="demo-simple-select-label">Currency</InputLabel><Select labelId="demo-simple-select-label" id="demo-simple-select" value={currencyselect} onChange={changeCurrency} sx={{ width: '100%', height:36 , mb: 2 }}>{currency.map((cur) => (<MenuItem value={cur.id} key={cur.id}>{cur.currency_name}</MenuItem>))}</Select></Col>
-                            <Col lg={4}><AvField name="remarks" label="Remarks" type="text" required onChange={handleChange} value={values.remarks}/></Col>
-                            <Col lg={4}><InputLabel id="demo-simple-select-label">Active</InputLabel><Select labelId="demo-simple-select-label" id="demo-simple-select" value={activeselect} onChange={changeActive} sx={{ width: '100%', height:36 , mb: 2 }}><MenuItem value={1}>Active</MenuItem><MenuItem value={0}>Inactive</MenuItem></Select></Col>  
-                        </Row>
-                        <Grid md={12} sx={{ textAlign: 'right' }}>
-                            <Button color="danger" type="submit" style={{ marginLeft: 15 }} onClick={onBack}>
-                                Back
-                            </Button>
-                            <Button color="primary" type="submit" style={{ marginLeft: 15 }}>
-                                Submit
-                            </Button>
-                        </Grid>
-                    </AvForm>
-                </CardBody>
-            </Card>
->>>>>>> 0810c3a5dd00a9220853ddd1f41ac7fba6289ccf
         </React.Fragment >
     );
 }
